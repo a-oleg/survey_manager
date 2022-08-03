cf94e9bb9fc601d9d9c6013437801b68368dcb61
Oleg <oleg.arhipovski@gmail.com>
2022-08-03 18:38:20 +0300

    Add code for create DB

--- src/main/java/com/github/a_oleg/SurveyManagerApplication.java
+++ src/main/java/com/github/a_oleg/SurveyManagerApplication.java
@@ -14,13 +14,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SurveyManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SurveyManagerApplication.class, args);
-        //Нужны ли строки ниже? Или нужно перенести их в пакет Service? Если да, то этот объект может понадобится в разных классах, может, его вынести куда-то в общем?
-        Configuration configuration = new Configuration();
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-
-        session.getTransaction().commit();
-        sessionFactory.close();
     }
 }
--- src/main/java/com/github/a_oleg/repository/SurveyRepository.java
+++ src/main/java/com/github/a_oleg/repository/SurveyRepository.java
@@ -1,9 +1,16 @@
 package com.github.a_oleg.repository;
 
+import com.github.a_oleg.entity.Survey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface SurveyRepository extends JpaRepository {
+import java.util.Set;
 
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Integer> {
+    Set<Survey> findById(int id);
+    Set<Survey> findByCreator(String creator);
+    Set<Survey> findBySurveyName(String surveyName);
+    Set<Survey> findByFolderId(int folderId);
+    Set<Survey> findByStatus(boolean status);
 }
