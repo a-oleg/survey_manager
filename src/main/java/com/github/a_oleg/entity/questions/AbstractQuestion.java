a910774ba58415577409166845355890680e6e09
Oleg <oleg.arhipovski@gmail.com>
2022-08-11 23:59:08 +0300

    Refactor question

--- src/main/java/com/github/a_oleg/entity/questions/AbstractQuestion.java
+++ src/main/java/com/github/a_oleg/entity/questions/AbstractQuestion.java
@@ -1,5 +1,8 @@
 package com.github.a_oleg.entity.questions;
 public abstract class AbstractQuestion {
     int id;
+    int surveyId;
     String textQuestion;
+    String descriptionText;
+    String buttonText;
 }
---
+++ src/main/java/com/github/a_oleg/entity/questions/QuestionWithTextAnswer.java
@@ -0,0 +1,6 @@
+package com.github.a_oleg.entity.questions;
+
+public class QuestionWithTextAnswer extends AbstractQuestion {
+    String responsePrefixText;
+    String responsePostfixText;
+}
