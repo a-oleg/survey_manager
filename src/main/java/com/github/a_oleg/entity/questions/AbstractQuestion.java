6896bc47d58af93f028a94bbbea3cb21368dbf5d
Oleg <oleg.arhipovski@gmail.com>
2022-08-14 15:35:00 +0300

    Add cod for question with text answer

--- src/main/java/com/github/a_oleg/entity/questions/AbstractQuestion.java
+++ src/main/java/com/github/a_oleg/entity/questions/AbstractQuestion.java
@@ -1,8 +1,44 @@
 package com.github.a_oleg.entity.questions;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public abstract class AbstractQuestion {
-    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    int questionId;
+    @Column(name = "survey_id")
     int surveyId;
+    @Column(name = "folder_id")
+    int folderId;
+    @Column(name = "type_question")
+    String typeQuestion;
+    @Column(name = "text_question")
     String textQuestion;
+    @Column(name = "description_text")
     String descriptionText;
+    @Column(name = "button_text")
     String buttonText;
+
+    public AbstractQuestion(int questionId, int surveyId, int folderId, String typeQuestion, String textQuestion, String descriptionText, String buttonText) {
+        this.questionId = questionId;
+        this.surveyId = surveyId;
+        this.folderId = folderId;
+        this.typeQuestion = typeQuestion;
+        this.textQuestion = textQuestion;
+        this.descriptionText = descriptionText;
+        this.buttonText = buttonText;
+    }
+
+    public AbstractQuestion(int surveyId, int folderId, String typeQuestion, String textQuestion, String descriptionText, String buttonText) {
+        this.surveyId = surveyId;
+        this.folderId = folderId;
+        this.typeQuestion = typeQuestion;
+        this.textQuestion = textQuestion;
+        this.descriptionText = descriptionText;
+        this.buttonText = buttonText;
+    }
 }
--- src/main/java/com/github/a_oleg/entity/questions/QuestionWithTextAnswer.java
+++ src/main/java/com/github/a_oleg/entity/questions/QuestionWithTextAnswer.java
@@ -1,6 +1,22 @@
 package com.github.a_oleg.entity.questions;
 
+import javax.persistence.Column;
+
 public class QuestionWithTextAnswer extends AbstractQuestion {
+    @Column(name = "response_prefix_text")
     String responsePrefixText;
+    @Column(name = "response_postfix_text")
     String responsePostfixText;
+
+    public QuestionWithTextAnswer(int questionId, int surveyId, int folderId, String typeQuestion, String textQuestion, String descriptionText, String buttonText, String responsePrefixText, String responsePostfixText) {
+        super(questionId, surveyId, folderId, typeQuestion, textQuestion, descriptionText, buttonText);
+        this.responsePrefixText = responsePrefixText;
+        this.responsePostfixText = responsePostfixText;
+    }
+
+    public QuestionWithTextAnswer(int surveyId, int folderId, String typeQuestion, String textQuestion, String descriptionText, String buttonText, String responsePrefixText, String responsePostfixText) {
+        super(surveyId, folderId, typeQuestion, textQuestion, descriptionText, buttonText);
+        this.responsePrefixText = responsePrefixText;
+        this.responsePostfixText = responsePostfixText;
+    }
 }
