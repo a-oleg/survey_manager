eecc89a39b81f3ed112782aefd00c505821a49f9
Oleg <oleg.arhipovski@gmail.com>
2022-09-14 21:08:28 +0300

    Add code for create table question_rating

--- src/main/resources/database/initDB.sql
+++ src/main/resources/database/initDB.sql
@@ -31,3 +31,30 @@ CREATE TABLE IF NOT EXISTS question_rating
     figure_type VARCHAR(50) NOT NULL,
     gradient_type VARCHAR(50) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS question_rating
+(
+    question_id BIGSERIAL PRIMARY KEY,
+    survey_id INTEGER REFERENCES surveys(survey_id),
+    question_number_in_the_survey INTEGER NOT NULL,
+    type_question VARCHAR(50) NOT NULL,
+    text_question VARCHAR(255) NOT NULL,
+    description_text VARCHAR(255) NOT NULL,
+    button_text VARCHAR(255) NOT NULL,
+    count_of_scale_elements INTEGER NOT NULL,
+    figure_type VARCHAR(50) NOT NULL,
+    gradient_type VARCHAR(50) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS question_slider
+(
+    question_id BIGSERIAL PRIMARY KEY,
+    survey_id INTEGER REFERENCES surveys(survey_id),
+    question_number_in_the_survey INTEGER NOT NULL,
+    type_question VARCHAR(50) NOT NULL,
+    text_question VARCHAR(255) NOT NULL,
+    description_text VARCHAR(255) NOT NULL,
+    button_text VARCHAR(255) NOT NULL,
+    type_slider VARCHAR(50) NOT NULL,
+    count_of_scale_elements INTEGER NOT NULL,
+    default_scale_element_number INTEGER NOT NULL,
+    figure_type VARCHAR(50) NOT NULL
+);
