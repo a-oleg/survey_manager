0e6cded69afa48c36936326943020d905c930c5b
Oleg <oleg.arhipovski@gmail.com>
2022-09-27 19:26:41 +0300

    Refactor QuestionScaleOfOpinion

--- src/main/java/com/github/a_oleg/entity/questions/QuestionScaleOfOpinion.java
+++ src/main/java/com/github/a_oleg/entity/questions/QuestionScaleOfOpinion.java
@@ -11,8 +11,14 @@ import java.util.HashMap;
 @Entity
 @Table(name = "question_scale_of_opinion")
 public class QuestionScaleOfOpinion extends AbstractQuestion {
-    //Как в БД укладывать элементы HashMap
+    //Как в БД укладывать элементы HashMap + как организовать суб.вопросы
     HashMap<Integer, String> scoreAndDescriptionScore;
+    @Column(name = "have_button_i_find_it_difficult_to_answer")
+    boolean haveButtonIFindItDifficultToAnswer;
+    @Column(name = "rating_for_entering_comment")
+    int ratingForEnteringComment;
+    @Column(name = "text_in_the_comment_field")
+    String textInTheCommentField;
     @Column(name = "figure_type")
     FigureType figureType;
     @Column(name = "gradient_type")
