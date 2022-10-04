8e269a100ba1df109461b1c8d4033e391c1bda9b
Oleg <oleg.arhipovski@gmail.com>
2022-10-04 22:08:06 +0300

    Add code for QuestionChoiseOfMedia

---
+++ src/main/java/com/github/a_oleg/dto/questions/QuestionTheChoiceOfMediaDto.java
@@ -0,0 +1,5 @@
+package com.github.a_oleg.dto.questions;
+
+public class QuestionTheChoiceOfMediaDto extends AbstractQuestionDto {
+    String textInOtherField;
+}
---
+++ src/main/java/com/github/a_oleg/entity/choiceoption/ChoiceSubquestion.java
@@ -0,0 +1,23 @@
+package com.github.a_oleg.entity.choiceoption;
+
+import com.github.a_oleg.entity.questions.QuestionWithTextAnswer;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "subquestion")
+public class ChoiceSubquestion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subquestion_id")
+    int subquestionId;
+    @Column(name = "subquestion_text")
+    String subquestionText;
+    @Column(name = "prefix_text")
+    String prefixText;
+    @Column(name = "postfix_text")
+    String postfixText;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "question_id")
+    QuestionWithTextAnswer questionWithTextAnswer;
+}
